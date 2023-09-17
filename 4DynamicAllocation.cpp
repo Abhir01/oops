a87aee9f303ad90e30a267949ed7e450a1327d72
Abhir01 <abr07k@gmail.com>
2023-09-17 17:08:42 +0530

    dynamically object creation

---
+++ 4DynamicAllocation.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Girls{
+public:
+	int f;	
+};
+int main(){
+	Girls *g1 = new Girls; //dynamic allocation of object g1 of type Girls
+	
+	//assigning object
+	(*g1).f = 12;
+	g1->f = 11;
+	
+	//accessing object (both are same)
+	cout<<(*g1).f<<" ";
+	cout<<g1 -> f;
+return 0;	
+}
